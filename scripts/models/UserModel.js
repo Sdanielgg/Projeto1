77ca8a765b32de29cd8bf53aba0baf28dd101dba
samfernandes03 <samuel.pinho.fernandes@gmail.com>
2024-05-15 11:43:08 +0100

    switched directory

---
+++ scripts/models/UserModel.js
@@ -0,0 +1,13 @@
+class User {
+    constructor(nome, idade, sexo, localidade, email, username, password, status = "inactive", role = "regular") {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.localidade = localidade;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.status = status;
+        this.role = role;
+    }
+}
