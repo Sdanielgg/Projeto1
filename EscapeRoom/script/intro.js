33b986b585aa74e95b19ab3a18c01a75d513b1b8
Laxingo <goncalosilchaves@gmail.com>
2024-05-23 16:16:01 +0100

    Intro Acabada e  pagina de perigo do escape room

--- EscapeRoom/script/intro.js
+++ EscapeRoom/script/intro.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
     const centerRectangle = document.getElementById("center-rectangle");
     const clickableText = document.getElementById("clickable-text");
+    const cornerImage = document.getElementById("hoot");
+    const backgroundIMG = document.getElementById("background");
 
     centerRectangle.addEventListener("click", function() 
     {
-        if (clickableText.textContent === "Clique aqui para mudar o texto") 
+        if (clickableText.textContent === "Parece que temos um Vírus no nosso computador que nos impede de o usar! Tenta acabar com o vírus com os teus conhecimentos de programação!") 
             {
-                clickableText.textContent = "Novo texto!";
+                clickableText.textContent = "Boa sorte!";
+                cornerImage.src = "style/images/hootFeliz.png";
                 centerRectangle.addEventListener("click", function() 
-                {
-                    if (clickableText.textContent === "Novo texto!") 
-                        {
-                        clickableText.textContent = "Novissimo texto!";
-                        }
-                }
-    )}
-    }
-    );
-});
+    {
+        if (clickableText.textContent === "Boa sorte!") 
+            {
+                backgroundIMG.src = "style/images/room-virus.png";
+                clickableText.style.display = 'none';
+                cornerImage.style.display = 'none';
+                centerRectangle.style.display = 'none';
+                
+            }
+})
+            }
+})}
+
+);
