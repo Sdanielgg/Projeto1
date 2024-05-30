d6eea7f075a5b39d700fc7a0d0dccf269e995af4
Sdanielgg <40220220@esmad.ipp.pt>
2024-05-30 18:31:06 +0100

    Credentials change now works still need to add go back buttons and make it go to the default profile page after clicking confirm changes button, also added footer do other pages

---
+++ scripts/views/UserProfileView.js
@@ -0,0 +1,67 @@
+function UserDisplayName() {
+    const userName=document.getElementById("Username")
+    console.log(userName.innerHTML)
+
+    let users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const userIndex = users.findIndex(u => u.status==="active");
+
+    text=users[userIndex].username
+    userName.innerHTML=text
+
+}
+let userInfo=document.getElementById("uInfo")
+let userChanges=document.getElementById("uChanges")
+let avatarChanges=document.getElementById("avatarChanges")
+let buttonUserEdit=document.getElementById("userEdit")
+let buttonAvatarChange=document.getElementById("avatarChange")
+
+
+
+UserDisplayName()
+
+
+function changeCredentials() {
+    const newUsername = document.querySelector('#Confs input[placeholder="Username"]').value;
+    const newPassword = document.querySelector('#Confs input[placeholder="Password"]').value;
+    const confirmPassword =document.querySelector('#Confs input[placeholder="Confirm Password"]').value;
+
+    if (newUsername !== null && newUsername !== "" && newPassword !== null && newPassword === confirmPassword){
+        let users = JSON.parse(localStorage.getItem('users')) || [];
+
+        const userIndex = users.findIndex(u => u.status==="active");
+
+
+        if (userIndex !== -1) {
+                users[userIndex].username=newUsername
+                users[userIndex].password=newPassword
+                localStorage.setItem('users', JSON.stringify(users));
+                handleLoginSuccess();
+            }
+        }else{
+            console.log("whoops nao deu :(")
+        }
+    }
+
+    
+    
+    function wait(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      
+      async function handleLoginSuccess() {
+        displayAlert('Login successful!', 'success');
+        
+        await wait(1000); // Wait for 1 second (1000 milliseconds)
+        
+        window.location.href = "./profile.html"
+      }
+document.getElementById("confirmButton").addEventListener("click", changeCredentials);
+buttonUserEdit.addEventListener("click",(event)=>{
+    userInfo.style.display="none"
+    userChanges.style.display="flex";
+    })
+buttonAvatarChange.addEventListener("click",(event)=>{
+    userInfo.style.display="none";
+    avatarChanges.style.display="flex"
+})
