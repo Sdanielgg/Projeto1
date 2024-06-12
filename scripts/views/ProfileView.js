0747b4e8f740cf4e2113eb0ac1d57bdee6fdf841
Sdanielgg <40220220@esmad.ipp.pt>
2024-06-12 18:37:32 +0100

    user is able to go through the site without login(still needs to block escape room)

--- scripts/views/ProfileView.js
+++ scripts/views/ProfileView.js
@@ -1,12 +1,17 @@
 function setUserAvatar() {
     let users = JSON.parse(localStorage.getItem("users")) || [];
     const userIndex = users.findIndex(u => u.status === "active");
-
+    const avatarImg=document.getElementById("avatar")
+    const loginRegisterButton=document.getElementById("loginRegisterButton")
+    loginRegisterButton.classList.remove("visible")
+    loginRegisterButton.classList.add("hidden")
     if (userIndex !== -1) {
         let newAvatarSrc = users[userIndex].avatar;
         document.getElementById("avatar").src = newAvatarSrc;
     } else {
-        console.log("No active user found.");
+        avatarImg.style.display="none"
+        loginRegisterButton.classList.remove('hidden');
+        loginRegisterButton.classList.add('visible');
     }
 }
 
