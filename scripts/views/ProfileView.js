a4d78185a74cf2aad62df5594492f44445aa6f51
Sdanielgg <40220220@esmad.ipp.pt>
2024-06-14 23:16:17 +0100

    fixed the user not logged in buttons to not allow users to go to the escape room while not logged in, missing the alert to tell the user to login

--- scripts/views/ProfileView.js
+++ scripts/views/ProfileView.js
@@ -1,8 +1,10 @@
 function setUserAvatar() {
     let users = JSON.parse(localStorage.getItem("users")) || [];
+
     const userIndex = users.findIndex(u => u.status === "active");
     const avatarImg=document.getElementById("avatar")
     const loginRegisterButton=document.getElementById("loginRegisterButton")
+
     if (userIndex !== -1) {
         let newAvatarSrc = users[userIndex].avatar;
         document.getElementById("avatar").src = newAvatarSrc;
@@ -11,6 +13,21 @@ function setUserAvatar() {
     } else {
         loginRegisterButton.style.display="block"
         avatarImg.style.display="none"
+    }
+}
+
+// function to check if the user is logged in and to give him access to the escape room will show alert if alerting the user to login to be able to use the escape room
+
+function escapeRoomStatus(){
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const userIndex = users.findIndex(u => u.status === "active");
+
+    if (userIndex !== -1) {
+        window.location="./EscapeRoom/intro.html"
+    } else {
+        console.log("user not logged in")
+        // samuel faz as tuas alerts aqui se puderes :)
     }
 }
 
